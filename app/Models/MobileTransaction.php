1b5aef3d46d93b40ec960c1b9cf2cacbb82cd6c2
Schneider Shades <schneidershades@gmail.com>
2020-11-01 02:46:57 +0100

    models

---
+++ app/Models/MobileTransaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MobileTransaction extends Model
+{
+    use HasFactory;
+}
