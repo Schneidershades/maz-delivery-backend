0fffc3dfe72b98787b97b04fbe011cd2f58f1409
Schneider Shades <schneidershades@gmail.com>
2020-11-01 02:46:40 +0100

    provider

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,28 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('identifier')->nullable();
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('lat')->nullable();
+            $table->string('lng')->nullable();
+            $table->foreignId('city_id')->constrained('cities')->nullable();
+            $table->foreignId('state_id')->constrained('states')->nullable();
+            $table->foreignId('country_id')->constrained('countries')->nullable();
+
+            $table->string('type')->nullable();
+            // if type is a company
+            $table->integer('staff_id')->unsigned()->index()->nullable();
+            $table->boolean('staff_type')->nullable();
+            $table->boolean('staff_vehicle_id')->unsigned()->index()->nullable();
+            $table->boolean('active')->default(true)->nullable();
+            $table->boolean('api')->default(false)->nullable();
+            $table->boolean('notification')->default(false)->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
