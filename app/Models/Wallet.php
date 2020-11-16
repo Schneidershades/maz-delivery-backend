e29822b9a71f3c08e33363e7b2dda298d6c5c3f9
Schneider Shades <schneidershades@gmail.com>
2020-11-16 06:24:37 +0100

    s

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Resources\Wallet\WalletResource;
 use App\Http\Resources\Wallet\WalletCollection;
+use App\Models\User;
 
 class Wallet extends Model
 {
@@ -13,5 +14,13 @@ class Wallet extends Model
 
     public $oneItem = WalletResource::class;
     public $allItems = WalletCollection::class;
+
+
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
 
 }
