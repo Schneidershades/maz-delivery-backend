63140ed28a62208d710d9e9d3a14ccd62e9bc583
Schneider Shades <schneidershades@gmail.com>
2020-11-16 06:40:31 +0100

    n

--- app/Http/Resources/Wallet/WalletResource.php
+++ app/Http/Resources/Wallet/WalletResource.php
@@ -15,7 +15,6 @@ class WalletResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
             'rydecoin' => $this->rydecoin,
         ];
     }
