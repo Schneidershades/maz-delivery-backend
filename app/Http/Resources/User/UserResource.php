3e02b765ac03c936c67b9ae14e73e187d015af22
Schneider Shades <schneidershades@gmail.com>
2020-11-16 06:26:57 +0100

    xe

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -19,9 +19,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'first_name' => $this->first_name,
-            'middle_name' => $this->middle_name,
-            'last_name' => $this->last_name,
+            'name' => $this->name,
             'email' => $this->email,
             'phone' => $this->phone,
             'address' => $this->address,
@@ -42,11 +40,11 @@ class UserResource extends JsonResource
             // 'addresses' => AddressResource::collection($this->addresses),
             // 'wallet' => WalletResource::collection($this->wallet),
 
-            // $this->mergeWhen($this->roles->first() != 'user', [
-            //     'permissions' => $this->getAllPermissions()->pluck('name')->map(function($permission){
-            //         return explode('_', $permission);
-            //     })->toArray(),
-            // ]),
+            $this->mergeWhen($this->roles->first() != 'user', [
+                'permissions' => $this->getAllPermissions()->pluck('name')->map(function($permission){
+                    return explode('_', $permission);
+                })->toArray(),
+            ]),
         ];
     }
 }
